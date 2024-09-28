820e68372c7b621403b16bd4213c2751460115e3
rawiwanpotikul <rawiwan.p11@gmail.com>
2024-09-28 11:20:12 +0700

    s

--- sensor.js
+++ sensor.js
@@ -1,5 +1,4 @@
 function updateData() {
-    // แทนที่ด้วยโค้ดสำหรับดึงข้อมูลของคุณ
     fetch('https://206.189.146.138/api/sensors')
     .then(response => response.json())
     .then(data => {
@@ -12,4 +11,3 @@ function updateData() {
   }
   setInterval(updateData, 1000); // เรียกใช้ฟังก์ชัน updateData ทุก 1000 มิลลิวินาที (1 วินาที)
   
-  // <script src="ตั้งชื่อไฟล์ว่าไรก็ใส่ตามนั้นไอบื้อ.js"></script> วิธีเรียกหา js
